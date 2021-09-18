235bb03e13885fd5062f06282fd83ada2b127cda
arveymenon <arveymenon@gmail.com>
2021-09-18 11:18:48 +0530

    init

--- src/design_patterns/observer/tweets/tweet.ts
+++
@@ -1,9 +0,0 @@
-export default class Tweet{
-    author?: string
-    message: string
-
-    constructor(author: string, message: string){
-        this.author = author
-        this.message = message
-    }
-}
