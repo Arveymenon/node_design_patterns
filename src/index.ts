f88e6e244685c5033e9554285110ef2a1ce7c1a7
arveymenon <arveymenon@gmail.com>
2021-09-18 11:22:08 +0530

    update adapter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import express from 'express'
-import BuilderRun from './design_patterns/builder/builder'
-import { Factory } from './design_patterns/factory/factory'
-import RunSingleton from './design_patterns/singleton/singleton'
 
 const app = express()
 const port = 5000
@@ -9,10 +6,6 @@ app.get('/', (_, res) => {
   res.status(200).send()
 })
 
-// new RunSingleton()
 
-// new Factory()
-
-new BuilderRun()
 
 app.listen(port, () => console.log(`Running on port ${port}`))
