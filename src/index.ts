4af9363728b154753b88017722614a810cc9fab0
arveymenon <arveymenon@gmail.com>
2021-09-18 11:23:09 +0530

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import express from 'express'
 import BuilderRun from './design_patterns/builder/builder'
-import { Factory } from './design_patterns/factory/factory'
-import RunSingleton from './design_patterns/singleton/singleton'
 
 const app = express()
 const port = 5000
 app.get('/', (_, res) => {
   res.status(200).send()
 })
-
-// new RunSingleton()
-
-// new Factory()
 
 new BuilderRun()
 
